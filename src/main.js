a143d963738c7fa5c159e68bd52141b9f35f47db
omar <doctoromar253@gmail.com>
2025-03-07 16:54:12 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import './style.css'
+import javascriptLogo from './logo.jpg'
+import viteLogo from './gatara-logo.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://gatara.org" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="" />
+    </a>
+    <a href="/" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="" style="border-radius: 3rem;"/>
+    </a>
+    <h1>Omar Gatara</h1>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
